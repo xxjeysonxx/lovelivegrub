7c953ff706bc986ca82b671bf5136b07eaa915f2
SooraMaru <48187538+xxjeysonxx@users.noreply.github.com>
2021-07-11 00:08:01 -0600

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,7 @@
+echo "# lovelivegrub" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/xxjeysonxx/lovelivegrub.git
+git push -u origin main
